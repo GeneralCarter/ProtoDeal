93cd5a388986fcf386b9bcc878af678d08e5da90
Jonathan Carter <jcarter.jc8@gmail.com>
2020-07-18 09:05:01 -0400

    Add IdentityServer Project

---
+++ src/PD.IdentityServer/src/IdentityServer/Config.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static string APIClientSecret => Configuration["APIClientSecret"];
+        public static string GoogleClientId => Configuration["GoogleClientId"];
+        public static string GoogleClientSecrect => Configuration["GoogleClientSecrect"];
+        public static IConfiguration Configuration;
+
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            {
+                new ApiResource
+                {
+                    Name = "pd_api",
+                    DisplayName = "Precision Deal Api",
+                    UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Email },
+                    Scopes =
+                    {
+                        new Scope()
+                        {
+                            Name = "pd_api",
+                            DisplayName = "pd_api",
+                        }
+                    }
+                }
+            };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+                new Client
+                {
+                    ClientId = "APIClient",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret(APIClientSecret.Sha256()),
+                        //new Secret("fwfw892lfw362".Sha256())
+                    },
+
+                    AllowedScopes = { 
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "pd_api"
+                    }
+                },
+                new Client
+                {
+                    ClientId = "frontentclient",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    AllowOfflineAccess = true,
+                    AccessTokenLifetime = 90, // 1.5 minutes
+                    AbsoluteRefreshTokenLifetime = 0,
+                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+                    UpdateAccessTokenClaimsOnRefresh = true,
+                    RequireConsent = false,
+
+                    RedirectUris = {
+                        "http://localhost:8080/callback",
+                        "http://localhost:8080/static/silent-renew.html"
+                    },
+                    PostLogoutRedirectUris = { "http://localhost:8080/" },
+                    AllowedCorsOrigins =     { "http://localhost:8080" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "pd_api"
+                    }
+}
+            };
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Alice"),
+                        new Claim("website", "https://alice.com")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("name", "Bob"),
+                        new Claim("website", "https://bob.com")
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ src/PD.IdentityServer/src/IdentityServer/Startup.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Environment = environment;
+            Configuration = configuration;
+            Config.Configuration = Configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.Ids)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(Config.GetUsers());
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+
+            services.AddAuthentication()
+                .AddGoogle("Google", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SaveTokens = true;
+                    options.ClientId = Config.GoogleClientId;
+                    options.ClientSecret = Config.GoogleClientSecrect;
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+               endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
